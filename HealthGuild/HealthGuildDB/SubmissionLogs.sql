56c3230ca1c5cdff74eac97b36139ceb12971ec7
eagunther <egunther@developerguild.com>
2016-03-05 20:54:40 -0500

    Created healthguild main viewer
    Created main viewer, file transmission tracker stubbed form, and
    database.

---
+++ HealthGuild/HealthGuildDB/SubmissionLogs.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[SubmissionLogs]
+(
+	[SubmissionLogID] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RunStartTime] DATETIME NOT NULL, 
+    [RunEndTime] DATETIME NOT NULL, 
+    [FileErrorCount] INT NOT NULL
+)
